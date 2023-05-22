ac9020f9eb5272d6f1a591d77091fd4b315113e2
Vance Wood <vancehwood@gmail.com>
2023-05-22 14:01:47 -0400

    Switching to langchain

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,15 +1,23 @@
 import { Configuration, OpenAIApi } from "openai";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+const openai2 = new OpenAI(configuration);
+
+process.env.OPENAI_API_KEY = configuration.apiKey;
+
 
 //get file to feed to prompt
 const axios = require('axios');
 const fs = require('fs');
-const filePath = 'C:\\Users\\TEMP\\Documents\\GitHub\\VanceGPT2\\message_1.json';
+const filePath = 'D:\\Software Development\\Github Repos\\openai-quickstart-node\\message_1.json';
 const fileContents = fs.readFileSync(filePath, 'utf8');
+
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
@@ -31,13 +39,24 @@ export default async function (req, res) {
     return;
   }
 
+
+
   try {
+
+    const model = new OpenAI({ temperature: 0.9 });
+    //const template = "What is a good name for a company that makes {product}?";
+    const prompt = new PromptTemplate({
+      template: generatePrompt(animal),
+      //inputVariables: ["product"],
+    });
+    /*
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
       max_tokens: 300
     });
+    **/
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
@@ -55,9 +74,11 @@ export default async function (req, res) {
   }
 }
 
+//  Here is some sample messages of his ${fileContents} 
+
 function generatePrompt(animal) {
   return `Imagine a 24 year old white Franco-American new yorker named Vance. 
-    Here is some sample messages of his ${fileContents} 
+  
     He is also Jewish, has a computer science degree, likes watching TV, rock and roll music, rap music, traveling and eating.
     He used to be a saber fencer in college.
     He's born in 1999 in Washington DC. Also it is now 2023.
